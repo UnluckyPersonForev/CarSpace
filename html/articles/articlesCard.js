d65cf056593a774dfbe341bba2000bdf0f4a9c66
UnluckyPersonForev <cs909920@gmail.com>
2025-04-27 22:00:37 -0400

    fixed butttons

--- html/articles/articlesCard.js
+++ html/articles/articlesCard.js
@@ -1,5 +1,13 @@
-const newsdata = [
-    { id: 1, title: "news1", content: "fdfdfd" },
-    { id: 2, title: "news1", content: "fdfdfd" },
-    { id: 2, title: "news1", content: "fdfdfd" },
-];
+// const newsdata = [
+//     {
+//         id: 1,
+//         title: "news1",
+//         title: "Какой срок службы коробки автомат и как его увеличить?",
+//         day: "04",
+//         month: "фев",
+//         year: "2022",
+//         content: "fdfdfd",
+//     },
+//     { id: 2, title: "news1", content: "fdfdfd" },
+//     { id: 2, title: "news1", content: "fdfdfd" },
+// ];
